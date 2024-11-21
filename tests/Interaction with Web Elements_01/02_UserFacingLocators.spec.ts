32d9cb614e469f70f0b1ead5c1af82a81a6e4cf6
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2024-11-21 12:18:49 +0600

    Learned few User Facing Locators

--- tests/Interaction with Web Elements_01/02_UserFacingLocators.spec.ts
+++ tests/Interaction with Web Elements_01/02_UserFacingLocators.spec.ts
@@ -5,3 +5,17 @@ test.beforeEach(async({page})=>{
   await page.getByText('Forms').click()
   await page.getByText('Form Layout').click()
 })
+
+test('02_User Facing Locator',async({page})=>{
+  //getByRole
+  await page.getByRole('button',{name:'Submit'}).first().click()
+  //getByLabel
+  await page.getByLabel('Email').first().fill('fardinahosan@gmail.com')
+  //getByPlaceholder
+  await page.getByPlaceholder('First Name').fill('Esha')
+  //getByText
+  await page.getByText('Website').click()
+  //getByTitle
+  await page.getByTitle('Extra Components').click()
+  //getByTestId (Not - User Facing) - (data-testid)
+})
