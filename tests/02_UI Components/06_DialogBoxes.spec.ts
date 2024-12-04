ba96a9ff78764efa4ec4bcf0ef78bb0d556f74a8
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2024-12-04 08:59:03 +0600

    Update Dialogbox
    01_DialogBoxes  - By Artemis Bandor Practice Session
    02_JavaScriptD_Dialogs - By Me using : https://practice.expandtesting.com/js-dialogs

--- tests/02_UI Components/06_DialogBoxes.spec.ts
+++ tests/02_UI Components/06_DialogBoxes.spec.ts
@@ -3,25 +3,75 @@ import { filter } from 'rxjs/operators';
 
 
 test.describe('04_List&Dropdowns',()=>{
-  test.beforeEach(async({page})=>{
+
+  test('01_DialogBoxes',async({page})=>{
     await page.goto('http://localhost:4200/')
-    await page.getByText('Modal & Overlays').click()
-    await page.getByText('Tooltip').click()
-  })
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
 
-  test('01_Tooltips',async({page})=>{
-    //const tooltip = await page.locator('nb-card',{hasText:'Tooltip Placements'})
-    const tooltip = await page.locator('nb-card nb-card-body')
-    await tooltip.getByText('Top').hover()
+    //My Method : find direct tr on this page that filter by unique text that tr has.
+    const table = await page.locator('tr').filter({hasText:'mdo@gmail.com'})
 
-    //Locator Assertions
-  //  const tooltipTexT = await page.locator('.cdk-overlay-container nb-tooltip')
-  //  await expect(tooltipTexT).toHaveText('This is a tooltip')
+    //Artemix Bandor Method : find table then find all tr under that tbale and among these tr, we will filter using unique text
+    const table02 = await page.getByRole('table').locator('tr',{hasText:'mdo'})
 
-   //Genereic Assertion
-   const tooltipTexT = await page.locator('.cdk-overlay-container nb-tooltip').textContent()
-   await expect(tooltipTexT).toEqual('This is a tooltip')
+    
 
+    //A Browser dialog box opend and we will perform Assertion and handle accept or dismiss or type on operation
+    page.on('dialog',farLog =>{
+      console.log(farLog.message())
+      expect(farLog.message()).toEqual('Are you sure you want to delete?')
+
+      farLog.accept()
+    })
+    //Click on Delete Icon
+    await table02.locator('.nb-trash').click()
+
+
+                           //My Made Assertion : 
+    //await expect(...): This is an assertion to validate conditions in Playwright tests.
+    //.toBeTruthy(): Checks if the value is truthy,
+    // which means the filtered locator must return at least one row without the text 'mdo'.
+    await expect(await page.getByRole('table').locator('tr',{hasNotText:'mdo'})).toBeTruthy()
+
+                           //Artemis Made Assertion -bestt
+    await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
   })
   
+
+
+
+  test('02_JavaScriptD_Dialogs',async({page})=>{
+
+    await page.goto('https://practice.expandtesting.com/js-dialogs')
+
+
+    
+    page.once('dialog',async(jsalert_01)=>{
+      await expect.soft(jsalert_01.message()).toEqual('I am a Js Alert')
+      await jsalert_01.accept()
+      await expect.soft(page.locator('#dialog-response')).toHaveText('OK')
+    })
+    await page.getByText('Js Alert').click()
+
+
+    
+    page.once('dialog',async(jsconfirm_01)=>{
+      await expect.soft(jsconfirm_01.message()).toEqual('I am a Js Confirm')
+      await jsconfirm_01.dismiss()
+      await expect.soft(page.locator('#dialog-response')).toHaveText('Cancel')
+    })
+    await page.getByText('Js Confirm').click()
+
+
+    
+    page.once('dialog',async(jsprompt_01)=>{
+      await expect.soft(jsprompt_01.message()).toEqual('I am a Js prompt')
+      await jsprompt_01.accept('fardinahosan')
+      await expect.soft(page.locator('#dialog-response')).toHaveText('fardinahosan')
+    })
+    await page.getByText('Js Prompt').click()
+
+
+  })
 })
