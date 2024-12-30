120c8175002d62d42c51f6f68f696be1e688e76c
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2024-12-30 08:26:16 +0600

    Basic POM Setup

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,14 +1,45 @@
 import { Page } from "@playwright/test";
 
-export class NavigationPage{
-    readonly page : Page
-    
-    constructor(page:Page){
-        this.page = page
-    }
+export class NavigationPage {
+  readonly onThisNavPage: Page;
 
-    async formLayoutsPage(){
-        await this.page.getByText('Forms').click()
-        await this.page.getByText('Form Layout').click()
+  constructor(page: Page) {
+    this.onThisNavPage = page;
+  }
+
+  async pageFormLayout() {
+    await this.navbarAction("Forms");
+    await this.onThisNavPage.getByText("Form Layout").click();
+  }
+
+  async pageDatePicker() {
+    await this.navbarAction("Forms");
+    await this.onThisNavPage.getByText("Datepicker").click();
+  }
+
+  async pageWebTable() {
+    await this.navbarAction("Tables & Data");
+    await this.onThisNavPage.getByText("Smart Table").click();
+  }
+
+  async pageToaster() {
+    await this.navbarAction("Modal & Overlays");
+    await this.onThisNavPage.getByText("Toastr").click();
+  }
+
+  async pageTooltip() {
+    await this.navbarAction("Modal & Overlays");
+    await this.onThisNavPage.getByText("Tooltip").click();
+  }
+
+  // Function to check if the navbar is already extended
+  private async navbarAction(buttonName: string) {
+    const buttonTitle = this.onThisNavPage.getByTitle(buttonName);
+    const navState = await buttonTitle.getAttribute("aria-expanded");
+    if (navState === "false") {
+      await buttonTitle.click();
+    } else {
+      console.log(`The navbar section '${buttonName}' is already expanded.`);
     }
+  }
 }
--- tests/userPageObject.spec.ts
+++ tests/userPageObject.spec.ts
@@ -1,6 +1,7 @@
 import {expect, test} from '@playwright/test'
 import { filter } from 'rxjs/operators';
-import {NavigationPage } from './../page-objects/navigationPage';
+import { NavigationPage } from './../page-objects/navigationPage';
+
 
 test.beforeEach(async({page})=>{
     await page.goto ('http://localhost:4200/')
@@ -8,6 +9,22 @@ test.beforeEach(async({page})=>{
 
   test('navigateToFormPage',async({page})=>{
     const navigateTo = new NavigationPage(page)
-    await navigateTo.formLayoutsPage()
-
+    await navigateTo.pageFormLayout()
+    await navigateTo.pageDatePicker()
   })
+
+  test('navigateTowebTablePage',async({page})=>{
+    const navigateTo = new NavigationPage(page)
+    await navigateTo.pageWebTable()
+  })
+
+  test('navigateToToasterPage',async({page})=>{
+    const navigateTo = new NavigationPage(page)
+    await navigateTo.pageToaster()
+  })
+
+  test('navigateToTooltipPage',async({page})=>{
+    const navigateTo = new NavigationPage(page)
+    await navigateTo.pageTooltip()
+  })
+
