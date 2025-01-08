60611c88f92e09d07aadaef2b2eb3703b3410ad0
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2025-01-08 08:47:25 +0600

    Revert "helperBase Setup Finish"
    This reverts commit fc1af5fc151d7068599e62e8de30c5ca000c5501.

--- page-objects/helperBase.ts
+++ page-objects/helperBase.ts
@@ -1,13 +1,9 @@
 import { Page } from "@playwright/test";    
 
 export class HelperBase{
-    protected readonly page:Page
-
+    readonly helper:Page
+    
     constructor(page:Page){
-        this.page=page
-    }
-
-    async waitForNumberOfSeconds(timeInSeconds:number){
-        await this.page.waitForTimeout(timeInSeconds * 1000)
+        this.helper=page
     }
 }
--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -1,41 +1,40 @@
 import { Page } from "@playwright/test";
-import { HelperBase } from "./helperBase";
 
-export class NavigationPage extends HelperBase {
-  
+export class NavigationPage {
+  readonly onThisNavPage: Page;
+
   constructor(page: Page) {
-    super(page)
+    this.onThisNavPage = page;
   }
 
   async pageFormLayout() {
     await this.navbarAction("Forms");
-    await this.page.getByText("Form Layout").click();
-    await this.waitForNumberOfSeconds(3)
+    await this.onThisNavPage.getByText("Form Layout").click();
   }
 
   async pageDatePicker() {
     await this.navbarAction("Forms");
-    await this.page.getByText("Datepicker").click();
+    await this.onThisNavPage.getByText("Datepicker").click();
   }
 
   async pageWebTable() {
     await this.navbarAction("Tables & Data");
-    await this.page.getByText("Smart Table").click();
+    await this.onThisNavPage.getByText("Smart Table").click();
   }
 
   async pageToaster() {
     await this.navbarAction("Modal & Overlays");
-    await this.page.getByText("Toastr").click();
+    await this.onThisNavPage.getByText("Toastr").click();
   }
 
   async pageTooltip() {
     await this.navbarAction("Modal & Overlays");
-    await this.page.getByText("Tooltip").click();
+    await this.onThisNavPage.getByText("Tooltip").click();
   }
 
   // Function to check if the navbar is already extended
   private async navbarAction(buttonName: string) {
-    const buttonTitle = this.page.getByTitle(buttonName);
+    const buttonTitle = this.onThisNavPage.getByTitle(buttonName);
     const navState = await buttonTitle.getAttribute("aria-expanded");
     if (navState === "false") {
       await buttonTitle.click();
