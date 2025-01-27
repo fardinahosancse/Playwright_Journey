c977a5f1579be91597ef32ced79cbf3a97159765
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2025-01-27 10:00:44 +0600

    Getting gobalURL fixers

--- tests/testWithFixtures.spec.ts
+++ tests/testWithFixtures.spec.ts
@@ -1,51 +1,27 @@
-import {expect, test} from '@playwright/test'
+import {expect} from '@playwright/test'
 import { filter, timeout } from 'rxjs/operators';
-import { NavigationPage } from './../page-objects/navigationPage';
-import { FormLayoutPage } from '../page-objects/formLayoutPage';
-import { time } from 'console';
-import { DatePickerPage } from '../page-objects/datePickerPage';
+import { PageManager } from '../page-objects/pageManager';
+import { test } from '../test-options';
 import { TestDataGenerator } from '../utility/testDataGenerator';
 
 
-test.beforeEach(async({page})=>{
-    await page.goto ('/')
+test.beforeEach(async({page,globalURL})=>{
+    await page.goto (globalURL)
   })
 
-
   test('VerifyFillingUsingTheGirdwithRequiredData',async({page})=>{
-    const navigateTo = new NavigationPage(page)
-    const onFormLayoutPage = new FormLayoutPage(page)
-    await navigateTo.pageFormLayout()
+    const pm = new PageManager(page)
     //test data
     const randomPassword = TestDataGenerator.generatePassword()
     const randomEmail = TestDataGenerator.generateEmail();
+    const randomFULLName = TestDataGenerator.generateFullName();
 
-    await expect.soft(await page.url()).toEqual('http://localhost:4200/pages/forms/layouts')
-    await onFormLayoutPage.submitUsingTheGirdWithCredentialAndSelectOption(randomEmail,randomPassword,'Option 1')
+   //navigating and filling the form
+    await pm.navigateTo().pageFormLayout()
+    await pm.onFormLayoutPage().submitUsingTheGirdWithCredentialAndSelectOption(randomEmail,randomPassword,'Option 1')
+    await pm.onFormLayoutPage().submitInlineForm(randomFULLName,randomEmail,'Remember me',false)
   })
 
-  test('VerifyFillingInlineFormwithRequiredData',async({page})=>{
-    const navigateTo = new NavigationPage(page)
-    const onFormLayoutPage = new FormLayoutPage(page)
-    await navigateTo.pageFormLayout()
-    await timeout(2000)
-    await expect.soft(await page.url()).toEqual('http://localhost:4200/pages/forms/layouts')
-    await onFormLayoutPage.submitInlineForm('Fardin Ahosan','fardinahosan@gmail.com','Remember me',false)
-  })
-
-  test('VerifyCommonDateSelectionFroDatePickerPage',async({page})=>{
-    const navigateTo = new NavigationPage(page)
-    const onDatePickerPage = new DatePickerPage(page)
-    await navigateTo.pageDatePicker()
-    await onDatePickerPage.selectCommonDatePickerFromToday(450)
-  })
-
-  test('VerifyRangeDateSelectionFromDatePickerPage',async({page})=>{
-    const navigateTo = new NavigationPage(page)
-    const onDatePickerPage = new DatePickerPage(page)
-    await navigateTo.pageDatePicker()
-    await onDatePickerPage.selectDatewitRangeFromToday(5,10)
-  })  
 
 
 
