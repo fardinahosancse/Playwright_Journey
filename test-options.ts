60e24dbb9856ce0f5cad70b31291422f4103119c
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2025-01-27 22:31:23 +0600

    TestFixtureSetup

--- test-options.ts
+++ test-options.ts
@@ -1,6 +1,13 @@
 import {test as base} from '@playwright/test'
 
-export type TestOptions = { globalURL: string }
+
+export type TestOptions = { globalURL: string,formLayoutPage:string }
 export const test = base.extend<TestOptions>({
-    globalURL: ['', {option:true}]
+    globalURL: ['', {option:true}],
+    formLayoutPage: [async({page},use)=>{
+        await page.goto ("/")
+        await page.getByText("Forms").click()
+        await page.getByText("Form Layout").click();
+        await use("")
+    },{auto:true}]
 }) 
--- tests/testWithFixtures.spec.ts
+++ tests/testWithFixtures.spec.ts
@@ -1,15 +1,13 @@
-import {expect} from '@playwright/test'
-import { filter, timeout } from 'rxjs/operators';
 import { PageManager } from '../page-objects/pageManager';
 import { test } from '../test-options';
 import { TestDataGenerator } from '../utility/testDataGenerator';
 
 
-test.beforeEach(async({page,globalURL})=>{
-    await page.goto (globalURL)
-  })
+// test.beforeEach(async({page,globalURL})=>{
+//     await page.goto (globalURL)
+//   })
 
-  test('VerifyFillingUsingTheGirdwithRequiredData',async({page})=>{
+  test('VerifyFillingUsingTheGirdwithRequiredData',async({page,formLayoutPage})=>{
     const pm = new PageManager(page)
     //test data
     const randomPassword = TestDataGenerator.generatePassword()
@@ -17,7 +15,7 @@ test.beforeEach(async({page,globalURL})=>{
     const randomFULLName = TestDataGenerator.generateFullName();
 
    //navigating and filling the form
-    await pm.navigateTo().pageFormLayout()
+    // await pm.navigateTo().pageFormLayout()
     await pm.onFormLayoutPage().submitUsingTheGirdWithCredentialAndSelectOption(randomEmail,randomPassword,'Option 1')
     await pm.onFormLayoutPage().submitInlineForm(randomFULLName,randomEmail,'Remember me',false)
   })
