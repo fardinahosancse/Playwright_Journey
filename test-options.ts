99208ae7015db1a97c863bff4d2be08b4bc0d309
S.M.Fardin Ahosan <57011344+fardinahosancse@users.noreply.github.com>
2025-01-28 18:37:02 +0600

    Page Manager as Fixture

--- test-options.ts
+++ test-options.ts
@@ -1,13 +1,27 @@
 import {test as base} from '@playwright/test'
+import { PageManager } from './page-objects/pageManager'
 
+export type TestOptions = { 
+    globalURL: string,
+    formLayoutPage:string,
+    pageManager:PageManager,
 
-export type TestOptions = { globalURL: string,formLayoutPage:string }
+ }
 export const test = base.extend<TestOptions>({
+    //Global URL Fixture
     globalURL: ['', {option:true}],
-    formLayoutPage: [async({page},use)=>{
-        await page.goto ("/")
+
+    //Form Layout Page Fixture
+    formLayoutPage: [async({page,globalURL},use)=>{
+        await page.goto (globalURL)
         await page.getByText("Forms").click()
         await page.getByText("Form Layout").click();
         await use("")
-    },{auto:true}]
+    },{auto:true}],
+     
+    //Page Manager Fixture
+    pageManager: async({page},use)=>{
+        const pm = new PageManager(page)
+        await use(pm)
+    },
 }) 
--- tests/testWithFixtures.spec.ts
+++ tests/testWithFixtures.spec.ts
@@ -1,26 +1,11 @@
-import { PageManager } from '../page-objects/pageManager';
 import { test } from '../test-options';
 import { TestDataGenerator } from '../utility/testDataGenerator';
 
-
-// test.beforeEach(async({page,globalURL})=>{
-//     await page.goto (globalURL)
-//   })
-
-  test('VerifyFillingUsingTheGirdwithRequiredData',async({page,formLayoutPage})=>{
-    const pm = new PageManager(page)
-    //test data
-    const randomPassword = TestDataGenerator.generatePassword()
+test('VerifyFillingUsingTheGirdwithRequiredData', async({ pageManager }) => {
+    const randomPassword = TestDataGenerator.generatePassword();
     const randomEmail = TestDataGenerator.generateEmail();
     const randomFULLName = TestDataGenerator.generateFullName();
 
-   //navigating and filling the form
-    // await pm.navigateTo().pageFormLayout()
-    await pm.onFormLayoutPage().submitUsingTheGirdWithCredentialAndSelectOption(randomEmail,randomPassword,'Option 1')
-    await pm.onFormLayoutPage().submitInlineForm(randomFULLName,randomEmail,'Remember me',false)
-  })
-
-
-
-
-
+    await pageManager.onFormLayoutPage().submitUsingTheGirdWithCredentialAndSelectOption(randomEmail, randomPassword, 'Option 1');
+    await pageManager.onFormLayoutPage().submitInlineForm(randomFULLName, randomEmail, 'Remember me', false);
+});
